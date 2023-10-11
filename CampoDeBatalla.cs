b74210059571a15ce6144aa63b7249882652d9a6
The-Man-M <143039832+The-Man-M@users.noreply.github.com>
2023-10-11 01:07:36 -0600

    version 2.00

--- CampoDeBatalla.cs
+++ CampoDeBatalla.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,32 +16,35 @@ namespace Paint_Bool
 
 
 
-        public CampoDeBatalla(List<JugadorPaintBall> equipo_1, List<JugadorPaintBall> equipo_2)
+        public CampoDeBatalla(List<JugadorPaintBall> equipo_1, List<JugadorPaintBall> equipo_2,string _eqipo1,string _equipo2)
         {
 
-            Equipo_1.Add("Equipo 1", equipo_1);
+            Equipo_1.Add(_eqipo1, equipo_1);
 
-            Equipo_2.Add("Equipo 2", equipo_2);
+            Equipo_2.Add(_equipo2, equipo_2);
 
         }
         public CampoDeBatalla() { }
         public void ComenzarBatalla()
         {
+           int ronda = 0;
             while (!HayGanador())
             {
+                ronda++;
+                nombredeljuago(ronda);
 
                 var equipo1Jugadores = Equipo_1.Values.First(); // Obtiene la lista de jugadores del primer equipo
                 var equipo2Jugadores = Equipo_2.Values.First(); // Obtiene la lista de jugadores del segundo equipo
 
                 var jugadoresEnPar = equipo1Jugadores.Zip(equipo2Jugadores, (jugador1, jugador2) => new { Jugador1 = jugador1, Jugador2 = jugador2 });
 
-                
+
 
                 foreach (var pareja in jugadoresEnPar)
                 {
                     pareja.Jugador1.correr();
                     pareja.Jugador2.correr();
-                    if(ramd()%2==0)
+                    if (ramd() % 2 == 0)
                     {
                         Console.ForegroundColor = ConsoleColor.Cyan;
                         pareja.Jugador1.disparar(pareja.Jugador2);
@@ -57,16 +61,31 @@ namespace Paint_Bool
                     Console.ForegroundColor = ConsoleColor.White;
                 }
                 Console.BackgroundColor = ConsoleColor.Black;
-                Console.WriteLine("Jugadores eliminados\n");
-               
-                foreach (var pareja in jugadoresEnPar)
+                Console.WriteLine("Ingrese exactamente \"Si\" para ver a los jugadores eliminados, de lo contrario presione \"Enter\"");
+                if (Console.ReadLine() == "Si")
                 {
-                    if(!pareja.Jugador1.activo ) pareja.Jugador1.jugadorEliminado();
-                   
-                    if(!pareja.Jugador2.activo) pareja.Jugador2.jugadorEliminado();
+                    Console.WriteLine("Jugadores eliminados\n");
+
+                    
+                    foreach(var equipo in Equipo_1)
+                    {
+                        Console.WriteLine($"Equipo: {equipo.Key}\n");
+                        foreach(var valor in equipo.Value)
+                        {
+                            if (!valor.activo) valor.jugadorEliminado();
+                        }
+                    }
+                    foreach (var equipo in Equipo_2)
+                    {
+                        Console.WriteLine($"Equipo: {equipo.Key}\n");
+                        foreach (var valor in equipo.Value)
+                        {
+                            if (!valor.activo) valor.jugadorEliminado();
+                        }
+                    }
+                    
                 }
-
-
+               
             }
             TerminarBatalla();
         }
@@ -95,17 +114,32 @@ namespace Paint_Bool
             foreach (var kvp in Equipo_2) foreach (var jugador in kvp.Value) if (jugador.activo) equipo2++;
 
 
-            //     if (equipo1 > equipo2) return true; else if (equipo1 < equipo2) return true;
+
             if (equipo1 == 0) return true;
             if (equipo2 == 0) return true;
 
             return false;
         }
         int ramd()
-    {
-       Random random = new Random();
-            return random.Next(1,100);
-    }
+        {
+            Random random = new Random();
+            return random.Next(1, 100);
+        }
+         void nombredeljuago(int i)
+        {
+            Console.Clear();
+            Console.WriteLine("  _______________________________________________________________________________");
+            Console.WriteLine("||_______________________________________________________________________________||");
+            Console.WriteLine("||                                                                               ||");
+            Console.WriteLine("|| * * *    * * *   * * *   *     *   * * * * *   * *      * * *   *       *     ||");
+            Console.WriteLine("|| *   *    *   *     *     * *   *       *       *   *    *   *   *       *     ||");
+            Console.WriteLine("|| * * *    * * *     *     *  *  *       *       * *      * * *   *       *     ||");
+            Console.WriteLine("|| *        *   *     *     *   * *       *       *   *    *   *   *       *     ||");
+            Console.WriteLine("|| *        *   *   * * *   *     *       *       * *      *   *   * * *   * * * ||");
+            Console.WriteLine("||_______________________________________________________________________________||");
+            Console.WriteLine("||_______________________________________________________________________________||\n\n");
+            Console.WriteLine($"Ronda numero {i}\n");
+        }
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Paint_Bool
+﻿using System;
+
+namespace Paint_Bool
 {
     internal class Program
     {
@@ -6,12 +8,24 @@
         {
             do
             {
+                string _equipo_1 = "Equipo 1";
+                string _equipo_2 = "Equipo 2";
+                Console.WriteLine("  _______________________________________________________________________________");
+                Console.WriteLine("||_______________________________________________________________________________||");
+                Console.WriteLine("||                                                                               ||");
+                Console.WriteLine("|| * * *    * * *   * * *   *     *   * * * * *   * *      * * *   *       *     ||");
+                Console.WriteLine("|| *   *    *   *     *     * *   *       *       *   *    *   *   *       *     ||");
+                Console.WriteLine("|| * * *    * * *     *     *  *  *       *       * *      * * *   *       *     ||");
+                Console.WriteLine("|| *        *   *     *     *   * *       *       *   *    *   *   *       *     ||");
+                Console.WriteLine("|| *        *   *   * * *   *     *       *       * *      *   *   * * *   * * * ||");
+                Console.WriteLine("||_______________________________________________________________________________||");
+                Console.WriteLine("||_______________________________________________________________________________||\n\n");
 
                 List<JugadorPaintBall> equipo_1 = new List<JugadorPaintBall>
               {
-                new JugadorPaintBall("jugador 1" ,"Equipo 1", 25, true, 50),
-                new JugadorPaintBall("jugador 2" ,"Equipo 1", 28, true, 50),
-                new JugadorPaintBall("jugador 3" , "Equipo 1", 22, true, 50)
+                new JugadorPaintBall("jugador 1", "Equipo 1", 25, true, 50),
+                new JugadorPaintBall("jugador 2", "Equipo 1", 28, true, 50),
+                new JugadorPaintBall("jugador 3", "Equipo 1", 22, true, 50)
               };
 
                 List<JugadorPaintBall> equipo_2 = new List<JugadorPaintBall>
@@ -21,19 +35,85 @@
                 new JugadorPaintBall("jugador 3", "Equipo 2", 22, true, 50),
                 };
 
+                Console.WriteLine("Ingrese exactamente \"SI\" para poder dar nombre a los equipos he integrantes de cada uno de estos\nPresione \"Enter\" para generar nombres");
 
-                CampoDeBatalla campo = new CampoDeBatalla(equipo_1, equipo_2);
+                if (Console.ReadLine() == "SI")
+                {
+                    Console.Clear();
+                    int _balas = balas();
+                    Console.Clear();
+                    _equipo_1 = equipo(1);
+                    Console.Clear();
+                    _equipo_2 = equipo(2);
+                    Console.Clear();
+
+                    equipo_1.Clear();
+                    equipo_2.Clear();
+                    for (int i = 0; i < 3; i++)
+                    {
+                        equipo_1.Add(new JugadorPaintBall(nombre(i + 1, _equipo_1), _equipo_1, edad(), true, _balas));
+                        Console.Clear();
+                    }
+                    for (int i = 0; i < 3; i++)
+                    {
+                        equipo_2.Add(new JugadorPaintBall(nombre(i + 1, _equipo_2), _equipo_1, edad(), true, _balas));
+                        Console.Clear();
+                    }
+                }
+                Console.Clear() ;
+                CampoDeBatalla campo = new CampoDeBatalla(equipo_1, equipo_2, _equipo_1, _equipo_2);
                 campo.ComenzarBatalla();
                 campo.HayGanador();
 
                 Console.ReadKey();
                 Console.Clear();
-            } while (true);
+                Console.WriteLine("Ingrese exactamente \"Si\" para salir, de lo contrario presione \"Enter\"");
+              
+            } while (Console.ReadLine() != "Si");
         }
+        static string nombre(int i, string equipo)
+        {
+            Console.Write($"Ingrese nombre del participante numero {i} del equipo {equipo}\n");
+            return Console.ReadLine();
+        }
+        static string equipo(int i)
+        {
 
+            Console.Write($"Ingrese nombre del equipo numero {i}: ");
+            return Console.ReadLine();
+        }
+        static uint edad()
+        {
+            uint numero;
+            do
+            {
+                Console.Write("Ingrese la edad\n");
+                Console.Write("Ingresa un número positivo: ");
 
+            } while (!uint.TryParse(Console.ReadLine(), out numero) || !Numero_Po(numero));
+            return numero;
 
+            static bool Numero_Po(uint numero)
+            {
+                return numero > 0;
+            }
+        }
+        static int balas()
+        {
+            int numero;
+            do
+            {
+                Console.Write("Cual sera la cantidad de balas?\n");
+                Console.Write("Ingresa un número positivo: ");
 
+            } while (!int.TryParse(Console.ReadLine(), out numero) || !Numero_Po(numero));
+            return numero;
+
+            static bool Numero_Po(int numero)
+            {
+                return numero > 0;
+            }
+        }
     }
 
 }
